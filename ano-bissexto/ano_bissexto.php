b8e88f480c3c7b26cc0895016cd9926fe72b0d9e
Juam Ferreira <juamferreira.c@gmail.com>
2016-01-26 18:31:59 -0200

    update ano_bissexto.php

--- ano-bissexto/ano_bissexto.php
+++ ano-bissexto/ano_bissexto.php
@@ -1,4 +1,20 @@
 <?php
+iniciaExec(); // inicia a função de cálculo do tempo da execução
+global $tempo;
+
+// Pega o tempo
+function pegaTempo(){
+	$microtime = explode(" ", microtime());
+	$tempo = $microtime[0] + $microtime[1];
+	return $tempo;
+}
+
+// Calcula o tempo do início
+function iniciaExec(){
+	global $tempo;
+	$tempo = pegaTempo();
+}
+
 function se_bissexto($ano){
 	if ( (($ano%4) == 0 && ($ano%100) != 0) || ($ano%400) == 0 ){
 		$bissexto = "O ano de ".$ano." é bissexto \n";
@@ -8,6 +24,15 @@ function se_bissexto($ano){
 
 	return $bissexto;
 }
+echo se_bissexto(2400); // Parâmetro receberá o ano a ser pesquisado
 
-echo se_bissexto(2016);
+// Calcula o fim da execução do script,
+// Apresenta o tempo de em ms
+function fimExec(){
+	global $tempo;
+	$fim = pegaTempo();
+	$tempoExec = $fim - $tempo;
+	return "Tempo de execução: ".number_format($tempoExec, 6)." ms \n";
+}
+echo fimExec();
 ?>
