d8ce18db0cdebab71421bcfc526424adaca8abb8
Juam Ferreira <juamferreira.c@gmail.com>
2016-01-26 22:51:22 -0200

    update miojos_do_joao.php

--- miojos-do-joao/miojos_do_joao.php
+++ miojos-do-joao/miojos_do_joao.php
@@ -8,9 +8,29 @@ function calcula($tempoPrep, $amp1, $amp2){
 			$amp1 = $amp2;
 			$amp2 = $maior;
 		}
-		for($amp2; $amp2 == 0; $amp2--){
-			$amp1--;
-			echo "amp1 = ".$amp1." amp2 = ".$amp2;
+		if ($amp1 == $amp2 || $amp2-$amp1 == 1 || $amp1-$tempoPrep == 1){
+			echo "<br/>Que pena!<br/>Não é possível cozinhar o miojo com as ampulhetas disponíveis.";
+		}else{
+			echo "Tempo de preparo = ".$tempoPrep."<br/>";
+			echo "Virou as duas ampulhetas<br/>";
+			for($i = $amp1; $i > 0; $i--){
+				echo "amp1 = ".$i.", amp2 = ".$amp2."<br/>";
+				$amp2--;
+			}
+			echo "amp1 esgotou, virou novamente<br/>";
+			for($j = $amp2; $j > 0; $j--){
+				echo "amp1 = ".$amp1.", amp2 = ".$j."<br/>";
+				$amp1--;
+			}
+			if ($amp1==$tempoPrep) {
+				echo "Começou a fazer o miojo<br/>";
+				for(; $amp1 > 0; $amp1--){
+					echo "amp1 = ".$amp1."<br/>";
+				}
+				echo "Miojo pronto, bom apetite!";
+			}else{
+				echo "<br/>Que pena!<br/>Não é possível cozinhar o miojo com as ampulhetas disponíveis.";
+			}
 		}
 	}
 }
