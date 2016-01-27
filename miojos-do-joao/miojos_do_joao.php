1268763e96aebd173dabb8c39786e80e23733a09
Juam Ferreira <juamferreira.c@gmail.com>
2016-01-26 23:46:18 -0200

    update miojos_do_joao.php
    Versão final da resposta ao desafio do miojo.

--- miojos-do-joao/miojos_do_joao.php
+++ miojos-do-joao/miojos_do_joao.php
@@ -1,38 +1,44 @@
 <?php
+
 function calcula($tempoPrep, $amp1, $amp2){
-	if($amp1<$tempoPrep || $amp2<$tempoPrep){
-		echo "Tempo da ampulheta menor que o tempo de preparo";
+	// Tempo da empulheta não pode ser menor que o tempo de preparo.
+	if($amp1<=$tempoPrep || $amp2<=$tempoPrep){
+		echo "Tempo da ampulheta não é maior que o tempo de preparo.\n";
 	}else{
-		if ($amp1>$amp2) {
+		// Coloca o número menor na variável $amp1.
+		if ($amp1>$amp2){
 			$maior = $amp1;
 			$amp1 = $amp2;
 			$amp2 = $maior;
 		}
+		// Testa se é possível cozinhar o miojo.
 		if ($amp1 == $amp2 || $amp2-$amp1 == 1 || $amp1-$tempoPrep == 1){
-			echo "<br/>Que pena!<br/>Não é possível cozinhar o miojo com as ampulhetas disponíveis.";
+			echo "\nQue pena!\nNão é possível cozinhar o miojo com as ampulhetas disponíveis.\n";
 		}else{
-			echo "Tempo de preparo = ".$tempoPrep."<br/>";
-			echo "Virou as duas ampulhetas<br/>";
+			echo "Tempo de preparo = ".$tempoPrep."\n";
+			echo "Virou as duas ampulhetas\n";
 			for($i = $amp1; $i > 0; $i--){
-				echo "amp1 = ".$i.", amp2 = ".$amp2."<br/>";
+				echo "ampulheta1 = ".$i.", ampulheta2 = ".$amp2."\n";
 				$amp2--;
 			}
-			echo "amp1 esgotou, virou novamente<br/>";
+			echo "amp1 esgotou, virou novamente\n";
+			//segunda virada da ampulheta que já terminou.
 			for($j = $amp2; $j > 0; $j--){
-				echo "amp1 = ".$amp1.", amp2 = ".$j."<br/>";
+				echo "ampulheta1 = ".$amp1.", ampulheta2 = ".$j."\n";
 				$amp1--;
 			}
+			// Neste ponto se a ampulheta1 atingir o mesmo valor do tempo de preparo continua a execução se não é encerrado.
 			if ($amp1==$tempoPrep) {
-				echo "Começou a fazer o miojo<br/>";
+				echo "Começou a fazer o miojo\n";
 				for(; $amp1 > 0; $amp1--){
-					echo "amp1 = ".$amp1."<br/>";
+					echo "ampulheta1 = ".$amp1."\n";
 				}
-				echo "Miojo pronto, bom apetite!";
+				echo "Miojo pronto, bom apetite!\n";
 			}else{
-				echo "<br/>Que pena!<br/>Não é possível cozinhar o miojo com as ampulhetas disponíveis.";
+				echo "\nQue pena!<br/>Não é possível cozinhar o miojo com as ampulhetas disponíveis.\n";
 			}
 		}
 	}
 }
-calcula(3, 5, 7);
+echo calcula(3, 5, 7); // informe no primeiro parâmetro o tempo de preraro, depois os tempos das ampulhetas.
 ?>
